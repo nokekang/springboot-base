97ab5e52e09072c8e57f02b00c28fff93b603951
nokekang <17621973193@163.com>
2018-10-26 21:41:55 +0800

    3333

--- src/main/java/org/gitconflict/test/Class3.java
+++ src/main/java/org/gitconflict/test/Class3.java
@@ -2,10 +2,5 @@ package org.gitconflict.test;
 
 public class Class3 {
 	public static void main(String[] args) {
-
-		System.out.println("33");
-		System.out.println("31");
-
-		System.out.println("31123");
 	}
 }
