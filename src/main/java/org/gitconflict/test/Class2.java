8195f21fcc0c54fd281a67bab692a54fc324798f
nokekang <17621973193@163.com>
2018-10-26 21:06:57 +0800

    aaa

---
+++ src/main/java/org/gitconflict/test/Class2.java
@@ -0,0 +1,7 @@
+package org.gitconflict.test;
+
+public class Class2 {
+	public static void main(String[] args) {
+		System.out.println("2");
+	}
+}
---
+++ src/main/java/org/gitconflict/test/Class3.java
@@ -0,0 +1,7 @@
+package org.gitconflict.test;
+
+public class Class3 {
+	public static void main(String[] args) {
+		System.out.println("3");
+	}
+}
